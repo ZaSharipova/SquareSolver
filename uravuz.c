0a3b72fcc9b9539e080758e136b5bdfb1e0a1619
ZaSharipova <sh.zarina.ild@gmail.com>
2025-08-17 17:46:30 +0300

    Изменение 2.1: изменила подписи

--- uravuz.c
+++ uravuz.c
@@ -2,8 +2,8 @@
 #include <math.h>
 
 float disc(float a, float b, float c);
-int roots2(float a, float b, float c, float discr);
 int find_res(float a, float b, float c);
+int roots2(float a, float b, float c, float discr);
 
 int main(void) {
     float a = 0, b = 0, c = 0;
@@ -18,25 +18,25 @@ float disc(float a, float b, float c) {
     return b * b - 4 * a * c;
 }
 
-int find_res(float a, float b, float c){
+int find_res(float a, float b, float c) {
     float discr = disc(a, b, c);
 
-    if (a == 0){
-        printf("leading coefficient doesn't has to be 0\n");
+    if (a == 0) {
+        printf("in the quadratic equation leading coefficient doesn't have to be 0\n");
         if (b != 0){
-            printf(" but in linear equation root is %.3f\n", -c/b);
+                printf("and in linear equation root is %.3f\n", -c/b);
         } else{
-            if (c == 0) { printf("but it has infinitely many roots\n");}
-            else { printf("but it has 0 roots\n");}
+            if (c == 0) { printf("and this equation has infinitely many roots\n");}
+            else { printf("and this equation has 0 roots\n");}
         }
-    } else{
-        if (discr < 0){
-            printf("has 0 roots\n");
+    } else {
+        if (discr < 0) {
+            printf("this equation has 0 roots\n");
         } else{
-            if (discr == 0){ 
-                printf("root is %.3f\n", -b/(2*a));
+            if (discr == 0) { 
+                printf("this equation's root is %.3f\n", -b/(2 * a));
             }
-            else{
+            else {
                 roots2(a, b, c, discr);
             }
         }
@@ -49,6 +49,6 @@ int roots2(float a, float b, float c, float discr) {
     float res1 = (-b + res0_sq) / (2 * a);
     float res2 = (-b - res0_sq) / (2 * a);
 
-    printf("roots are %.3f and %.3f\n", (res1 > res2 ? res2 : res1), (res1 > res2 ? res1 : res2));
+    printf("this equation's roots are %.3f and %.3f\n", (res1 > res2 ? res2 : res1), (res1 > res2 ? res1 : res2));
     return 0;
 }
